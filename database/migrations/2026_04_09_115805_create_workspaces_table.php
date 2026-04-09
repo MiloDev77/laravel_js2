2d431653a9eee4e8b6016867031510f0dfe5ff82
MiloDev77 <syamilgnugroho@gmail.com>
2026-04-09 19:08:03 +0700

    Menambahkan file api.php dan schema table untuk keperluan database

---
+++ database/migrations/2026_04_09_115805_create_workspaces_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workspaces', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id')->constrained(
+                table: 'users',
+                indexName: 'workspaces_user_id',
+            )->cascadeOnDelete();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workspaces');
+    }
+};
